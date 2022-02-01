fc7768543fa46f49955425aa40f89e5d62ad6e90
safoinme <safoinme@gmail.coml>
2022-02-01 23:13:32 +0100

    Modifying save change script to save changed on dataset

--- config_git.sh
+++ config_git.sh
@@ -24,9 +24,4 @@ git config user.email "${COMMIT_EMAIL}"
 #git remote add origin ${GIT_REPO}
 #it checkout -t origin ${GIT_BRANCH}
 
-# Configure dvc and pull dataset.
-#dvc remote add -d minikubeminio s3://dvc-bucket/
-#dvc remote modify minikubeminio endpointurl http://10.105.56.43:9000
-#dvc remote modify minikubeminio access_key_id "minio"
-#dvc remote modify minikubeminio secret_access_key "Do&BfNOtNcWqGtWV5i"
 dvc pull
--- save_changes.sh
+++ save_changes.sh
@@ -1,23 +1,31 @@
 #!/bin/sh
 
-#Add dataset folder changes
+#Add data changes to dvc (in this case it's dataset folder)
 dvc add dataset
-git add *.dvc
 
-# Check to see if there are changes
+# initialise found changes empty will be used later to check that there is a change
 CHANGES_FOUND=""
 
+# saving names of changed files in change using status command
 CHANGES=`git status -s | awk {'print $2'}`
-echo ${CHANGES}
 
+# check if no change is done , the code will exit and print that changes must be made since the script was called
 if [ -z "${CHANGES}" ]; then
 	echo "No changes detected."
 	echo "Must be changes on dataset folder"
     exit
 fi
 
-# Commit and push the detected changes if they are found.
-if [ ! -z "${CHANGES_FOUND}" ]; then
+# Add all changed files that end with .dvc to our git ! the objective of the script is to save changes on data not code that's why we only add .dvc
+for changed_file in ${CHANGES}; do
+	if [[ "${changed_file}" == "*.dvc" ]]; then
+		CHANGES_FOUND="1"
+		git add "${changed_file}"
+	fi
+done
+
+# Commit and push the detected changes if they are found. and push new dataset version to S3 Storage
+if [ ! -z "${CHANGES}" ]; then
 	echo "Changes detected."
     current="`date +'%Y-%m-%d %H:%M:%S'`"
     message="Dataset detected changes: $current"
@@ -25,4 +33,3 @@ if [ ! -z "${CHANGES_FOUND}" ]; then
     dvc push
 	git push origin argo
 fi
-
